42c8f7c3d2d38f83143b824ffed9eb3f123c17ba
Alyssa Wendt <alyssa95wendt@gmail.com>
2024-05-25 20:31:10 -0400

    Refactor project structure and implement ThemeContext
    - Remove unused App.scss
    - Implement ThemeContext for theme management
    - Update App.tsx to integrate ThemeContext
    - Delete unused index.scss
    - Modify index.tsx to remove references to deleted files
    - Remove unnecessary react-app-env.d.ts
    - Remove reportWebVitals.ts
    - Remove setupTests.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useEffect } from 'react';
+import { ThemeProvider, useTheme } from './context/ThemeContext'
 
-function App() {
+const App: React.FC = () => {
+  const { theme } = useTheme()
+
+  useEffect(() => {
+    document.body.className = theme
+  }, [theme])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the Dark Mode App</h1> 
     </div>
-  );
+  )
 }
 
-export default App;
+const WrapperApp: React.FC = () => (
+<ThemeProvider>
+  <App />
+</ThemeProvider>
+)
+
+export default WrapperApp;
